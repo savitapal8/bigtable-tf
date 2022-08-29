8ea5b8033aeecc709c51c78e573cdad2d3878570
Savita Pal <savita.pal08@gmail.com>
2022-08-29 13:19:42 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,11 +1,13 @@
 provider "google" {
   project = "modular-scout-345114"
   region  = "us-central1"
+  access_token = var.access_token
   #credentials = file("./ca-key.json")
 }
 
 provider "google-beta" {
   project = "modular-scout-345114"
   region  = "us-central1"
+  access_token = var.access_token
   #credentials = file("./ca-key.json")
 }
